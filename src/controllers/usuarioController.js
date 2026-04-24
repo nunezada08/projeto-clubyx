89535aacb6c0433d334a2a1f169c0823a2871580
Henrico <henrico.ferreira@aluno.senai.br>
2026-04-24 10:28:57 -0300

    feat: cria o CRUD de Usuários

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,131 @@
+import UsuarioModel from '../models/UsuarioModel';
+
+export const criar = async (req, res) => {
+    try {
+        if (!req.body) {
+            return res.status(400).json({ error: 'Corpo da requisição vazio. Envie os dados!' });
+        }
+
+        const { nome, email, senha } = req.body;
+
+        if (!nome){
+            return res.status(400).json({ error: 'O campo "nome" é obrigatório!' });
+        }
+
+        if (!email){
+            return res.status(400).json({ error: 'O campo "email" é obrigatório!' });
+        }
+        if (!email.includes('@')) {
+            return res.status(400).json({error: 'O campo "senha" precisa ter um @ (ex: joao@email.com) '})
+        }
+
+        if (!senha) {
+            return res.status(400).json({error: 'O campo "senha" é obrigatório'})
+        }
+
+
+        const usuario = new UsuarioModel({ nome, emal, senha });
+        const data = await usuario.criar();
+
+        return res.status(201).json({ message: 'Usuário criado com sucesso!', data });
+    } catch (error) {
+        console.error('Erro ao criar:', error);
+        return res.status(500).json({ error: 'Erro interno ao salvar o Usuário.' });
+    }
+};
+
+export const buscarTodos = async (req, res) => {
+    try {
+        const registros = await UsuarioModel.buscarTodos(req.query);
+
+        if (!registros || registros.length === 0) {
+            return res.status(400).json({ message: 'Nenhum Usuário encontrado.' });
+        }
+
+        return res.status(200).json(registros);
+    } catch (error) {
+        console.error('Erro ao buscar:', error);
+        return res.status(500).json({ error: 'Erro ao buscar Usuários.' });
+    }
+};
+
+export const buscarPorId = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'O ID enviado não é um número válido.' });
+        }
+
+        const usuario = await UsuarioModel.buscarPorId(parseInt(id));
+
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuário não encontrado.' });
+        }
+
+        return res.status(200).json({ data: usuario });
+    } catch (error) {
+        console.error('Erro ao buscar:', error);
+        return res.status(500).json({ error: 'Erro ao buscar Usuário.' });
+    }
+};
+
+export const atualizar = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'ID inválido.' });
+        }
+
+        if (!req.body) {
+            return res.status(400).json({ error: 'Corpo da requisição vazio. Envie os dados!' });
+        }
+
+        const usuario = await UsuarioModel.buscarPorId(parseInt(id));
+
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuário não encontrado para atualizar.' });
+        }
+
+        if (req.body.nome !== undefined) {
+            usuario.nome = req.body.nome;
+        }
+        if (req.body.email !== undefined) {
+            usuario.email = req.body.email
+        }
+        if (req.body.senha !== undefined) {
+            usuario.senha = req.body.senha
+        }
+
+        const data = await usuario.atualizar();
+
+        return res.status(200).json({ message: `O Usuário "${data.nome}" foi atualizado com sucesso!`, data });
+    } catch (error) {
+        console.error('Erro ao atualizar:', error);
+        return res.status(500).json({ error: 'Erro ao atualizar Usuário.' });
+    }
+};
+
+export const deletar = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'ID inválido.' });
+        }
+
+        const usuario = await UsuarioModel.buscarPorId(parseInt(id));
+
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuário não encontrado para deletar.' });
+        }
+
+        await usuario.deletar();
+
+        return res.status(200).json({ message: `O Usuário "${usuario.nome}" foi deletado com sucesso!`, deletado: usuario });
+    } catch (error) {
+        console.error('Erro ao deletar:', error);
+        return res.status(500).json({ error: 'Erro ao deletar usuário.' });
+    }
+};
---
+++ src/models/UsuarioModel.js
@@ -0,0 +1,53 @@
+import prisma from '../lib/services/prismaClient.js';
+
+export default class UsuarioModel {
+    constructor({ id = null, nome, email, senha, numeroTelefone = null, foto = null} = {}) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.numeroTelefone = numeroTelefone;
+        this.foto = foto
+    }
+
+    async criar() {
+        return prisma.usuario.create({
+            data: {
+                nome: this.nome,
+                email: this.email,
+                senha: this.senha,
+            },
+        });
+    }
+
+    async atualizar() {
+        return prisma.usuario.update({
+            where: { id: this.id },
+            data: { nome: this.nome, email: this.email, senha: this.senha },
+        });
+    }
+
+    async deletar() {
+        return prisma.usuario.delete({ where: { id: this.id } });
+    }
+
+    static async buscarTodos(filtros = {}) {
+        const where = {};
+
+        if (filtros.nome) {
+            where.nome = { contains: filtros.nome, mode: 'insensitive' };
+        }
+        if (filtros.email) {
+            where.email = { contains: filtros.email, mode: 'insensitive'}
+        }
+        return prisma.exemplo.findMany({ where });
+    }
+
+    static async buscarPorId(id) {
+        const data = await prisma.usuario.findUnique({ where: { id } });
+        if (!data) {
+            return null;
+        }
+        return new UsuarioModel(data);
+    }
+}
